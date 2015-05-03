d866cd4c29746d38859f7d82cc63227a6ac0f1ee
Ubuntu <pat@pkm-php.pkm-php.j5.internal.cloudapp.net>
2015-05-03 18:08:35 +0000

    Big push: topic modeling, graph creation

--- scraper/scrapegoogle.py
+++ scraper/scrapegoogle.py
@@ -4,13 +4,20 @@ import urllib2
 import codecs
 import re
 import unicodedata
-from gensim import corpora,models,similarities
 from collections import defaultdict
 import operator
 import sys
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 import pickle
+import enchant
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import lda
+import numpy as np
+from sklearn.cluster import KMeans
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import math
 
 def scrape(phrase, num_results):
     #replace spaces with plusses to convert them for the url
@@ -45,12 +52,14 @@ def scrape(phrase, num_results):
   
     #make the HTML requests and store the results
     documents = []
+    to_remove = []
     for link in links:
         request = urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0'})
         try:
             html = urllib2.urlopen(request).read()
         except: #should figure out root cause here
             print "error opening", link
+            to_remove.append(link)
             continue
         soup = BeautifulSoup(html)
         #text = word_tokenize(text)
@@ -70,47 +79,18 @@ def scrape(phrase, num_results):
 
         #print text
         documents.append(text)
-        
-    return documents
+    for x in to_remove:
+        links.remove(x)
+    return documents, links
 
 
-def save_dict(docs):
-    pickle.dump(docs, open("save.p","wb"))
-
-def load_dict():
-    return pickle.load(open("save.p", "rb"))
+def save_dict(docs, links,query_nr):
+    pickle.dump(docs, open(query_nr + '.p',"wb"))
+    pickle.dump(links, open(query_nr + '_urls.p','wb'))
 
 
-
-"""This isn't quite working yet - still trying to figure out gensim, since I think it is 
-doing some sort of supervised topic modeling and I am only familar with unsupervised
-"""
-def topic_model(docs):
-    stoplist = set('for a of the and to in'.split())
-    
-    #get rid of common words (articles, prepositions, conjunctions
-    texts = [[word for word in document.lower().split() if word not in stoplist]  for document in docs]
-    
-    freq = defaultdict(int)
-    for text in texts:
-        for token in text:
-            freq[token] += 1
-    
-    #remove common  
-    texts = [[token for token in text if freq[token] > 1] for text in texts]
-
-    dictionary = corpora.Dictionary(texts)
-
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    corpora.MmCorpus.serialize('/tmp/corpus.mm', corpus)
-    corpus = corpora.MmCorpus('/tmp/corpus.mm')
-    lsi = models.lsimodel.LsiModel(corpus=corpus, num_topics = 10) 
-    print lsi
-    lsi.print_topics()
-    print("done")
-def sort_dict(x):
-    import operator
-    return sorted(x.items(), key=operator.itemgetter(1))
+def load_dict(fn):
+    return pickle.load(open(fn, "rb"))
 
 def count_words(docs):
     stop = set(stopwords.words('english'))
@@ -159,7 +139,7 @@ def make_cloud(docs):
     flat_doc = count_words(docs)
     from wordcloud import WordCloud
     import wordcloud
-    import matplotlib.pyplot as plt
+   
     wc = WordCloud(ranks_only = True, font_path='/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf')
     wc.fit_words(flat_doc)
     #print wc 
@@ -167,24 +147,142 @@ def make_cloud(docs):
     plt.imshow(wc)
     plt.axis("off")
     plt.show()
+   
+def clean_vec(docs, literal):
+    d = enchant.Dict("en_US")
+    stop = set(stopwords.words('english'))
+    stop = stop.union({'like','may'})
+    #literal = set(['data','science','scientist','scientists', 'data science', 'data scientist', 'data scientists'])
     
-if __name__ == "__main__":
+    literal = []
+    check_stop = lambda doc: word not in literal and word not in stop and len(word) > 2 and d.check(word)
+    to_ret = []
+    for doc in docs:
+        tmp = []
+        doc = doc.split()
+        for word in doc:
+            if check_stop(word):
+                tmp.append(word)
+        to_ret.append(tmp)
+    return to_ret
+
+def flatten_list(docs):
+    out = []
+    for doc in docs:
+        out.append(' '.join(word for word in doc))
+    return out
+
+def make_vectorizer(docs):
+    vec = CountVectorizer(min_df = 5, ngram_range=(1,2))
+    counts = vec.fit_transform(docs)
+    names = vec.get_feature_names()
+    return counts, names
+
+
+def make_tfidf_vec(docs):
+    vec = TfidfVectorizer(min_df=5, ngram_range=(1,2))
+    tfidf = vec.fit_transform(docs)
+    names = vec.get_feature_names()
+    return tfidf, names
+
+def run_kmeans(vector=None, links=[], iters=500, clusters=8):
+    km = KMeans(n_clusters=clusters, max_iters=iters)
+    km.fit_transform(vec)
+    clusters = defaultdict(list)
+    for i in xrange(len(links)):
+        clusters[km.labels[i]].append(links[i])
+    for x in clusters:
+        print x, clusters[x]
+    return km.labels_
+
+
+
+def topic_model(counts, names,iters=1500, topics = 6, rand_state=1 ):
+    model = lda.LDA(n_iter=iters, n_topics=topics, random_state=rand_state)
+    dist = model.fit_transform(counts)
+    print_topics(model, names)
+    return model, dist
+
+def print_topics(model, names):
+    topic_word = model.topic_word_
+    n_top_words = 20 
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = np.array(names)[np.argsort(topic_dist)][:-n_top_words:-1]
+        print("Topic {}: {}".format(i," | ".join(topic_words)))
+
+
+
+def assign_cluster(dist, links, n_clusters=6):
+    clusters = [[] for i  in xrange(n_clusters)]
+    for i in xrange(len(links)):
+        cluster = max( (v,j) for j,v in enumerate(dist[i]) ) [1]
+        clusters[cluster].append(links[i])
+    percents = []
+    for i in xrange(n_clusters):
+        percents.append(float(len(clusters[i]))/float(len(links)))
+    return clusters, percents
+
+def plot_bargraph(percents,width=.8):
+    topics = xrange(len(percents))
+    barlist = plt.bar(topics, percents, width=width)
+    plt.xticks(np.arange(len(topics)) + width/2.,topics)
+    plt.xlabel('Topic number')
+    plt.show()
+
+def find_matches(dist, links, thresh=.2):
+    edges = []
+    for i in xrange(len(links)):
+        for j in xrange(i+1, len(links)):
+            link1 = links[i]
+            link2 = links[j]
+            if determine_edge(dist[i],dist[j],thresh):
+                edges.append((link1,link2))
+    return edges
+
+def determine_edge(dist1,dist2,thresh):
+    diff = 0
+    for x,y in zip(dist1,dist2):
+        diff += abs(x-y)
+    return diff < thresh
+
+
+if  __name__ == "__main__":
+
+
     argc = len(sys.argv)
     if argc < 3:
         print "usage: python scrapegoogle.py <num_results> <search_query>"
         sys.exit(1)
+    #import resource
+    #resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
     num_res = int(sys.argv[1])
     query = ' '.join(sys.argv[i] for i in xrange(2, argc))
     print "Query:", query
     print "Number of results:", num_res
-    docs = scrape(query,num_res)
-    save_dict(docs)
+    print "starting to scrape"
+    literal = set(query.split())
+    docs, links = scrape(query,num_res)
+    print "saving dictionary"
+    query = query.replace(' ','_')
+    fn = query + '_' + str(num_res) + '.p'
+    import os
     
-    docs = load_dict()
-    flat_doc = count_words(docs)
-    #print flat_doc
-    #flat_doc = 'ac a a a a a a b b'
-    #words = [el[0] for el in flat_doc]
-    #counts = [el[1] for el in flat_doc]
-    #model = topic_model(docs)
-   
+    
+    print fn
+    save_dict(docs,links,query + '_' + str(num_res))
+    print "reloading dict"
+    links = load_dict(query + '_' + str(num_res) + '_urls.p')
+    docs = clean_vec(load_dict(fn), literal)
+    print "cleaning data"
+    flat_doc = flatten_list(docs)
+    print "vectorizing data"
+    counts, names = make_vectorizer(flat_doc)
+    print "topic modeling"
+    model,dist = topic_model(counts, names)
+    print len(links), links
+    clusters, percents = assign_cluster(dist, links)
+    plot_bargraph(percents)
+    #flat_doc = count_words(docs)
+    
+
+ 
